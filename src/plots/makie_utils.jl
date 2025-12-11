078e2783b2d0f3c5a4182aa821b3d65f275cacd8
Mauricio Vargas <38122573+m4wnn@users.noreply.github.com>
2025-12-11 13:39:18 -0600

    feat: adding a new method in dateticks for specific dates in a vector

--- src/plots/makie_utils.jl
+++ src/plots/makie_utils.jl
@@ -87,7 +87,7 @@ function tslines!(
         kwargs...,
     )
     # Set date ticks if not already set
-    dateticks!(ax, dates)
+    dateticks!(ax, first(dates):last(dates))
     return l
 end
 
@@ -104,14 +104,28 @@ Sets the current (or specified) axis to be DateTime with given steps and format.
 """
 function dateticks!(
         ax = current_axis(),
-        dates::AbstractArray{<:Date} = Date(2001, 1):Month(12):Date(2050, 1);
+        dates::StepRange{Date, R} = Date(2001, 1):Month(12):Date(2050, 1);
         step::DatePeriod = Month(12),
         format::AbstractString = "u-yyyy",
-    )
+    ) where {R <: DatePeriod}
     # Create dates for the X-axis
     date_ticks = first(dates):step:last(dates)
     numdates = datetime2rata.(date_ticks)
     strdates = Dates.format.(date_ticks, format, locale = "spanish")
+    # Set them for the current axis
+    ax.xticks = (numdates, strdates)
+    ax.xticklabelrotation = π / 4
+    return numdates, strdates
+end
+
+function dateticks!(
+        ax = current_axis(),
+        dates::AbstractArray{Date} = [Date(2001, 1):Month(12):Date(2050, 1)...];
+        format::AbstractString = "u-yyyy"
+    )
+    # Create dates for the X-axis
+    numdates = datetime2rata.(dates)
+    strdates = Dates.format.(dates, format, locale = "spanish")
     # Set them for the current axis
     ax.xticks = (numdates, strdates)
     ax.xticklabelrotation = π / 4
